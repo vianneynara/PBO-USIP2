efbf379ed0fd5495d521e54f617a52776a5e0e87
Circle <narwandaru.nara@gmail.com>
2023-05-31 15:31:58 +0700

    Updated documentation

--- src/program/Jawaban.java
+++ src/program/Jawaban.java
@@ -1,11 +1,17 @@
 package program;
 
+/**
+ * Kelas jawaban ini berisi label dari sebuah jawaban. Bisa digunakan untuk soal pilihan ganda dan isian.
+ * */
 public class Jawaban {
     private String label;
 
+    /** Constructor utama. */
     public Jawaban(String label) {
         this.label = label;
     }
+
+    /* Getters / Setters */
 
     public String getLabel() {
         return label;
--- src/program/MataKuliah.java
+++ src/program/MataKuliah.java
@@ -1,19 +1,21 @@
 package program;
 
+/**
+ * Kelas ini mengandung nama, fakultas, dan dosen dari sebuah mata kuliah.
+ * */
 public class MataKuliah {
-    private String fakultas;
     private String nama;
-    private String dosen;
-    private String semester;
-    private String tanggal;
+    private String fakultas;
+    private Dosen dosen;
 
-    public MataKuliah(String fakultas, String nama, String dosen, String semester, String tanggal) {
-        this.fakultas = fakultas;
+    /** Constructor utama. */
+    public MataKuliah(String nama, String fakultas, Dosen dosen) {
         this.nama = nama;
+        this.fakultas = fakultas;
         this.dosen = dosen;
-        this.semester = semester;
-        this.tanggal = tanggal;
     }
+
+    /* Getters */
 
     public String getNama() {
         return nama;
@@ -23,15 +25,53 @@ public class MataKuliah {
         return fakultas;
     }
 
-    public String getDosen() {
+    public Dosen getDosen() {
         return dosen;
     }
 
-    public String getSemester() {
-        return semester;
+    /* Setters */
+
+    public void setNama(String nama) {
+        this.nama = nama;
     }
 
-    public String getTanggal() {
-        return tanggal;
+    public void setFakultas(String fakultas) {
+        this.fakultas = fakultas;
+    }
+
+    public void setDosen(Dosen dosen) {
+        this.dosen = dosen;
+    }
+}
+
+/**
+ * Kelas ini menyimpan data dari sebuah dosen yaitu nama dan NIP.
+ * */
+class Dosen {
+    private String nama;
+    private String NIP;
+
+    /** Constructor utama. */
+    public Dosen(String nama, String NIP) {
+        this.nama = nama;
+        this.NIP = NIP;
+    }
+
+    /* Getters / Setters */
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNIP() {
+        return NIP;
+    }
+
+    public void setNIP(String NIP) {
+        this.NIP = NIP;
     }
 }
