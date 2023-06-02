fd35294870006a63324697b22f853e0cdbb6d5c9
Circle <narwandaru.nara@gmail.com>
2023-06-02 15:38:30 +0700

    Added constructor for SoalEsai

--- src/program/Jawaban.java
+++ src/program/Jawaban.java
@@ -4,11 +4,20 @@ package program;
  * Kelas jawaban ini berisi label dari sebuah jawaban. Bisa digunakan untuk soal pilihan ganda dan isian.
  * */
 public class Jawaban {
+    
+    private String namaFile;
+    private String linkFile;
     private String label;
 
-    /** Constructor utama. */
+    /** Constructor jawaban soal ganda dan isian. */
     public Jawaban(String label) {
         this.label = label;
+    }
+
+    /** Constructor jawaban soal esai. */
+    public Jawaban(String namaFile, String linkFile) {
+        this.namaFile = namaFile;
+        this.linkFile = linkFile;
     }
 
     /* Getters / Setters */
@@ -19,6 +28,22 @@ public class Jawaban {
 
     public void setLabel(String label) {
         this.label = label;
+    }
+
+    public String getNamaFile() {
+        return namaFile;
+    }
+
+    public void setNamaFile(String namaFile) {
+        this.namaFile = namaFile;
+    }
+
+    public String getLinkFile() {
+        return linkFile;
+    }
+
+    public void setLinkFile(String linkFile) {
+        this.linkFile = linkFile;
     }
 
     @Override
