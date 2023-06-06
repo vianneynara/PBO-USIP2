fd6630aa83f99d2449fd3b35c1e75d1f66c59af7
Circle <narwandaru.nara@gmail.com>
2023-06-06 16:50:58 +0700

    Ujian class now only contains data holder for an ujian.

--- src/program/Ujian.java
+++ src/program/Ujian.java
@@ -3,7 +3,11 @@ package program;
 import java.util.*;
 import java.util.stream.Stream;
 
+/**
+ * Menyimpan data mengenai sebuah ujian.
+ * */
 public class Ujian {
+
     private SoalUjian[] listSoal;
     private MataKuliah mataKuliah;
     private Dosen dosen;
@@ -11,12 +15,12 @@ public class Ujian {
     private String semester;
     private String tanggal;
 
-    /* Constructor utama. */
+    /** Constructor utama. */
     public Ujian(SoalUjian[] listSoal, MataKuliah mataKuliah, Dosen dosen, String namaUjian, String semester, String tanggal) {
         this.listSoal = Stream.of(
-             getSoalPilihanGanda(listSoal),
-             getSoalIsian(listSoal),
-             getSoalEsai(listSoal)
+             UjianUtility.getSoalPilihanGanda(listSoal),
+             UjianUtility.getSoalIsian(listSoal),
+             UjianUtility.getSoalEsai(listSoal)
         )
         /* Mengkonversi Stream menjadi Array bertipe SoalUjian. */
         .flatMap(Arrays::stream)
@@ -29,106 +33,55 @@ public class Ujian {
         this.tanggal = tanggal;
     }
 
-    /**
-     * Mengembalikan soal bertipe {@link SoalPilihanGanda} dalam bentuk array dari list soal bertipe {@link SoalUjian}.
-     * Proses dilakukan dengan mengiterasi setiap soal pada list soal dan dimasukkan ke dalam sebuah array.
-     * @param listSoal list soal bertipe {@link SoalUjian}
-     * @return Array of {@link SoalPilihanGanda}
-     * */
-    public static SoalPilihanGanda[] getSoalPilihanGanda(SoalUjian[] listSoal) {
-        ArrayList<SoalPilihanGanda> soalPilihanGanda = new ArrayList<>();
-        for (SoalUjian soal : listSoal)
-            if (soal instanceof SoalPilihanGanda) soalPilihanGanda.add((SoalPilihanGanda) soal);
-        return soalPilihanGanda.toArray(new SoalPilihanGanda[0]);
+    /** Getters */
+
+    public SoalUjian[] getListSoal() {
+        return listSoal;
     }
 
-    /**
-     * Mengembalikan soal bertipe {@link SoalIsian} dalam bentuk array dari list soal bertipe {@link SoalUjian}.
-     * Proses dilakukan dengan mengiterasi setiap soal pada list soal dan dimasukkan ke dalam sebuah array.
-     * @param listSoal list soal bertipe {@link SoalUjian}
-     * @return Array of {@link SoalIsian}
-     * */
-    public static SoalIsian[] getSoalIsian(SoalUjian[] listSoal) {
-        ArrayList<SoalIsian> soalIsian = new ArrayList<>();
-        for (SoalUjian soal : listSoal)
-            if (soal instanceof SoalIsian) soalIsian.add((SoalIsian) soal);
-        return soalIsian.toArray(new SoalIsian[0]);
+    public MataKuliah getMataKuliah() {
+        return mataKuliah;
     }
 
-    /**
-     * Mengembalikan soal bertipe {@link SoalEsai} dalam bentuk array dari list soal bertipe {@link SoalUjian}.
-     * Proses dilakukan dengan mengiterasi setiap soal pada list soal dan dimasukkan ke dalam sebuah array.
-     * @param listSoal list soal bertipe {@link SoalUjian}
-     * @return Array of {@link SoalEsai}
-     * */
-    public static SoalEsai[] getSoalEsai(SoalUjian[] listSoal) {
-        ArrayList<SoalEsai> soalEsai = new ArrayList<>();
-        for (SoalUjian soal : listSoal)
-            if (soal instanceof SoalEsai) soalEsai.add((SoalEsai) soal);
-        return soalEsai.toArray(new SoalEsai[0]);
+    public Dosen getDosen() {
+        return dosen;
     }
 
-    /**
-     * Metode utama untuk menjalankan ujian.
-     * */
-    public void startUjian() {
-        System.out.println("\t\t\t\tLEMBAR JAWAB MAHASISWA");
-        System.out.println("--------------------------------------------------");
-        System.out.println("Fakultas        : " + mataKuliah.getFakultas());
-        System.out.println("Matakuliah      : " + mataKuliah.getNama());
-        System.out.println("Nama dosen      : " + dosen.getNama());
-        System.out.println("Semester        : " + semester);
-        System.out.println("Hari / Tanggal  : " + tanggal);
-        System.out.println("Nama ujian      : " + namaUjian);
+    public String getNamaUjian() {
+        return namaUjian;
+    }
 
-        int amount = 0;    // menghitung jumlah soal
+    public String getSemester() {
+        return semester;
+    }
 
-        /* Array indikator soal pertama*/
-        boolean[] isFirst = new boolean[] {true, true, true};
-        /* List soal sudah dianggap terurut: Pilihan ganda - Isian - Esai. */
-        for (SoalUjian soal : listSoal) {
-            /* Ujian Pilihan ganda. */
-            if (soal instanceof SoalPilihanGanda pilgan) {
-                if (isFirst[0]) {
-                    System.out.println();
-                    System.out.println("===================================================");
-                    System.out.println("\t\t\t\tSoal Pilihan Ganda");
-                    System.out.println("===================================================");
-                    isFirst[0] = false;
-                }
-                System.out.printf("%02d. %s", ++amount, pilgan.getSoal());
-                pilgan.jawab();
-                System.out.println();
-            }
+    public String getTanggal() {
+        return tanggal;
+    }
 
-            /* Ujian Isian. */
-            if (soal instanceof SoalIsian isian) {
-                if (isFirst[1]) {
-                    System.out.println();
-                    System.out.println("===================================================");
-                    System.out.println("\t\t\t\t\tSoal Isian");
-                    System.out.println("===================================================");
-                    isFirst[1] = false;
-                }
-                System.out.printf("%02d. %s", ++amount, isian.getPertanyaan());
-                System.out.println();
-                isian.jawab();
-                System.out.println();
-            }
+    /** Setters */
 
-            /* Ujian Esai. */
-            if (soal instanceof SoalEsai esai) {
-                if (isFirst[2]) {
-                    System.out.println();
-                    System.out.println("===================================================");
-                    System.out.println("\t\t\t\t\tSoal Esai");
-                    System.out.println("===================================================");
-                    isFirst[2] = false;
-                }
-                System.out.printf("%02d. %s", ++amount, esai.getPertanyaan());
-                esai.jawab();
-                System.out.println();
-            }
-        }
+    public void setListSoal(SoalUjian[] listSoal) {
+        this.listSoal = listSoal;
+    }
+
+    public void setMataKuliah(MataKuliah mataKuliah) {
+        this.mataKuliah = mataKuliah;
+    }
+
+    public void setDosen(Dosen dosen) {
+        this.dosen = dosen;
+    }
+
+    public void setNamaUjian(String namaUjian) {
+        this.namaUjian = namaUjian;
+    }
+
+    public void setSemester(String semester) {
+        this.semester = semester;
+    }
+
+    public void setTanggal(String tanggal) {
+        this.tanggal = tanggal;
     }
 }
